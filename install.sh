c6136bd12510ec4367a061a1ba8f6414673362c0
shaoshitong <1090784053@qq.com>
2023-03-03 10:54:14 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+pip install --upgrade pip
+pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip install --upgrade jaxlib
+pip install --upgrade git+https://github.com/google/flax.git
+pip ml-collections tensorflow-gan tensorflow_io tensorflow_datasets tensorflow tensorflow-addons tensorboard absl-py
